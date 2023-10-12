d115e34d4f9bfb1a05944a7c48b5512811a4e601
yshafrir <yshafrir@gmail.com>
2023-10-12 13:09:18 -0400

    gof 3 - working version

--- dist/index.js
+++ dist/index.js
@@ -8,24 +8,17 @@ const board_1 = require("./board");
 const app = (0, express_1.default)();
 const port = 3200;
 /**
- Rules
- step in time, the following transitions occur:
- - Any live cell with fewer than two live neighbors dies, as if by underpopulated.
- - Any live cell with two or three live neighbors lives on to the next generation.
- - Any live cell with more than three live neighbors dies, as if by overpopulation.
- - Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
- These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
- - Any live cell with two or three live neighbors survives.
- - Any dead cell with three live neighbors becomes a live cell.
- - All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+ * Starts game of life board and plays a single turn
  */
-/**
- * Starts game of life board
- */
-app.post("/init", (req, res) => {
+app.post("/play", (req, res) => {
     const board = new board_1.GameBoard();
-    res.send("Initialized a Game of Life board...");
-    res.send(board.print());
+    console.log("Initial state of board:");
+    console.log(board.String());
+    console.log("Playing a turn in the game of life");
+    board.DoStep();
+    let result = board.String();
+    console.log(`Result: ${result}`);
+    res.send(result);
 });
 // Start the app
 app.listen(port, () => {
--- src/board.ts
+++ src/board.ts
@@ -1,48 +1,99 @@
 /**
- * Cell represents a cell in the Game of Life game board
- */
-class Cell {
-  cell: Number[];
-  alive: Number | null;
-
-  constructor(
-    ldu: Number,
-    u: Number,
-    rdu: Number,
-    l: Number,
-    r: Number,
-    ldd: Number,
-    d: Number,
-    rdd: Number
-  ) {
-    this.alive = null;
-    this.cell = [ldu, u, rdu, l, r, ldd, d, rdd];
-  }
-
-  print() {
-    let state: Number | String = this.alive === null ? "-" : this.alive
-    console.log(this.cell[0] + " " + this.cell[1] + " " + this.cell[2]);
-    console.log(this.cell[3] + " " + state + " " + this.cell[4]);
-    console.log(this.cell[5] + " " + this.cell[6] + " " + this.cell[7]);
-  }
-}
-
-/**
  * GameBoard represents the state of the Game of Life run
  */
 export class GameBoard {
-  board: Array<Cell>;
+  livingCells: [number, number][];
 
   constructor() {
-    this.board = new Array<Cell>();
-    this.board.push(new Cell(1, 1, 0, 0, 1, 1, 0, 0));
-    this.board.push(new Cell(0, 0, 0, 0, 1, 0, 0, 0));
-    this.board.push(new Cell(1, 0, 0, 0, 1, 0, 0, 0));
+    this.livingCells = [
+      [0, 2],
+      [-1, 2],
+      [0, 0],
+      [1, 5],
+    ];
   }
 
-  print() {
-    this.board.forEach(cell => {
-        cell.print()
+  /**
+     Rules
+    step in time, the following transitions occur:
+    - Any live cell with fewer than two live neighbors dies, as if by underpopulated.
+    - Any live cell with two or three live neighbors lives on to the next generation.
+    - Any live cell with more than three live neighbors dies, as if by overpopulation.
+    - Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
+    - Any live cell with two or three live neighbors survives.
+    - Any dead cell with three live neighbors becomes a live cell.
+    - All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+    */
+
+  DoStep() {
+    let resultBoard: [number, number][] = [];
+    let visited: [number, number][] = [];
+    const neighborLocations: [number, number][] = [
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [-1, 0],
+      [1, 0],
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+    ];
+
+    this.livingCells.forEach((cell) => {
+      let aliveNeighbors: number = 0;
+      // check each living cell's neighbor to determine if
+      // it will live of the next generation
+      neighborLocations.forEach((neighbor: [number, number]) => {
+        let neighborX = cell[0] + neighbor[0];
+        let neighborY = cell[1] + neighbor[1];
+        if (this.checkNeighborCell(neighborX, neighborY)) {
+          aliveNeighbors += 1;
+        } else if (
+          !visited.some((item) => item[0] == neighborX && item[1] == neighborY)
+        ) {
+          // Any dead cell with three live neighbors becomes a live cell.
+          let deadNeighborAliveNeighbors: number = 0;
+          neighborLocations.forEach((neighbor: [number, number]) => {
+            let x = neighborX + neighbor[0];
+            let y = neighborY + neighbor[1];
+            if (this.checkNeighborCell(x, y)) {
+              deadNeighborAliveNeighbors += 1;
+            }
+          });
+          if (deadNeighborAliveNeighbors === 3) {
+            resultBoard.push([neighborX, neighborY]);
+          }
+          visited.push([neighborX, neighborY]);
+        }
+      });
+
+      // Any live cell with two or three live neighbors survives.
+      if (aliveNeighbors === 2 || aliveNeighbors === 3) {
+        resultBoard.push(cell);
+      }
     });
+
+    let unique = new Set(resultBoard);
+    this.livingCells = [...unique];
+  }
+
+  checkNeighborCell(x: number, y: number): boolean {
+    for (let index = 0; index < this.livingCells.length; index++) {
+      const cell = this.livingCells[index];
+      if (cell[0] === x && cell[1] == y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  String(): string {
+    let result: string[] = [];
+    this.livingCells.forEach((cell) => {
+      result.push(`Living cell at x:${cell[0]}, y:${cell[1]}`);
+    });
+
+    return result.join("\r\n");
   }
 }
